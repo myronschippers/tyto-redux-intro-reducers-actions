2b1ff4e5709b353723a7b816b485f3fcdd4b10d3
Myron <dubtwee@gmail.com>
2019-12-10 13:10:54 -0600

    adding code for reducers and actions lecture

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class App extends Component {
+    state = {
+        dog: '',
+        cat: '',
+    }
+
     clickToDispatch = (event) => {
         // DISPATCH HERE
         this.props.dispatch({
-            type: 'WOOT',
+            type: 'WOOT_ONE',
+        });
+        this.props.dispatch({
+            type: 'WOOT_TWO',
         });
     }
 
+    addDog = (event) => {
+        this.props.dispatch({
+            type: 'ADD_DOG',
+            payload: this.state.dog
+        })
+    }
+
+    addCat = (event) => {
+        this.props.dispatch({
+            type: 'ADD_CAT',
+            payload: this.state.cat
+        })
+    }
+
+    changeAnimalField = (event, animal) => {
+        this.setState({
+            [animal]: event.target.value,
+        })
+    }
+
     render() {
+        // console.log('catReducer: ', this.props.store.catReducer);
+        // console.log('dogReducer: ', this.props.store.dogReducer);
+        const catElements = this.props.store.catReducer.map((item, index) => {
+            return <li key={index}>{item}</li>
+        });
+        const dogElements = this.props.store.dogReducer.map((item, index) => {
+            return <li key={index}>{item}</li>
+        });
         return (
             <div>
                 <header>
@@ -17,13 +54,43 @@ class App extends Component {
                 </header>
 
                 <div>
+                    <input
+                        placeholder="Dog's Name"
+                        type="text"
+                        onChange={(event) => this.changeAnimalField(event, 'dog')}
+                    />
+                    <button onClick={this.addDog}>Add Dog</button>
+                </div>
+
+                <div>
+                    <input
+                        placeholder="Cat's Name"
+                        type="text"
+                        onChange={(event) => this.changeAnimalField(event, 'cat')}
+                    />
+                    <button onClick={this.addCat}>Add Cat</button>
+                </div>
+
+                <div>
                     <p>Help me Redux, you're my only hope.</p>
                     <button onClick={this.clickToDispatch}>Dispatch ME</button>
                 </div>
+
+                <h2>Dog List</h2>
+                <ul>
+                    {/* RENDER DOGS */}
+                    {dogElements}
+                </ul>
+
+                <h2>Cat List</h2>
+                <ul>
+                    {/* RENDER CATS */}
+                    {catElements}
+                </ul>
             </div>
         );
     }
 }
 
 // currying
-export default connect()(App);
+export default connect(mapStoreToProps)(App);
--- src/index.js
+++ src/index.js
@@ -6,20 +6,74 @@ import * as serviceWorker from './serviceWorker';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
-const firstReducer = () => {
-    console.log(`Hey!!! I'm a reducer in the store.`);
-    return {};
+const firstReducer = (state={ kittyKat: 'Meow' }, action) => {
+    // action = {
+    //     type: ''
+    // }
+    
+    if (action.type === 'WOOT_ONE') {
+        console.log(`Hey!!! I'm a reducer in the store.`);
+        console.log('action:', action);
+        return {
+            ...state,
+            message: 'Totes WOOTed!!!',
+        };
+    } else if (action.type === 'NO_WOOT_ONE') {
+        return {
+            ...state,
+            message: '',
+        };
+    }
+    return state;
 };
 
-const secondReducer = () => {
-    console.log(`I'm a tea pot....`);
-    return {};
+const secondReducer = (state={}, action) => {
+    if (action.type === 'WOOT_TWO') {
+        console.log(`I'm a tea pot....`);
+        console.log('action:', action);
+        return {
+            ...state,
+            teaPot: 'Earl Grey, soooooo grey.',
+        }
+    } else if (action.type === 'NO_WOOT_TWO') {
+        return {
+            ...state,
+            teaPot: 'no tea :(',
+        }
+
+    } else {
+        return state;
+    }
+   
+    // return state;
 };
+
+const dogReducer = (state=[], action) => {
+    if (action.type === 'ADD_DOG') {
+        return [
+            ...state,
+            action.payload
+        ]
+    }
+    return state;
+}
+
+const catReducer = (state=[], action) => {
+    if (action.type === 'ADD_CAT') {
+        return [
+            ...state,
+            action.payload
+        ]
+    }
+    return state;
+}
 
 const storeInstance = createStore(
     combineReducers({
-        firstReducer,
-        secondReducer
+        firstReducer: firstReducer,
+        secondReducer,
+        catReducer,
+        dogReducer
     })
     // manages our state data model in redux
     
